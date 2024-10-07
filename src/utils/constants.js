9f7fa76bb6b8aca18f894354032e68a44a1a73de
sahithreddy05 <sahithreddy050101@gmail.com>
2024-10-07 12:58:54 -0400

    small bug fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: process.env.API_TOKEN,
+        Authorization: `Bearer process.env.API_TOKEN`,
     }
 };
 
